f248e3b02a4af15baa8d522c2e2f087d05e633b8
tarekfarouqq <tarekahmedfarouq@gmail.com>
2025-12-13 21:29:29 +0200

    implemented auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { AddressBookComponent } from './address-book/address-book.component';
 import { MainLayoutComponent } from './layout/main-layout.component';
 import { JobsComponent } from './jobs/jobs.component';
 import { DepartmentsComponent } from './departments/departments.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -13,6 +14,7 @@ export const routes: Routes = [
     {
         path: 'app',
         component: MainLayoutComponent,
+        canActivate: [authGuard],
         children: [
             { path: 'address-book', component: AddressBookComponent },
             { path: 'jobs', component: JobsComponent },
